070f783d6b848ccfec2671cad13fe9d9cd1b7621
Santiago Montoya <santiagomontoya997@gmail.com>
2026-08-13 20:33:43 -0500

    feat: project setup

---
+++ src/main/java/com/pqrs/PqrsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PqrsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PqrsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pqrs/PqrsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PqrsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
